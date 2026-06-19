3be7ab39850f1b62b5b55840499c5081b05a8a59
myself-Rishi-saha <rishisaha302016@gmail.com>
2026-06-20 01:31:55 +0530

    cors

--- newbackend/run.py
+++ newbackend/run.py
@@ -1,70 +1,102 @@
-# run.py
+# # run.py
 
-import os
-from dotenv import load_dotenv
-from app import create_app
-from flask_cors import CORS
-
-load_dotenv()
-app = create_app()
-
-
-if __name__ == "__main__":
-
-    host = os.getenv("HOST", "0.0.0.0")
-    port = int(os.getenv("PORT", 5000))
-    debug = os.getenv("FLASK_DEBUG", "False").lower() == "true"
-
-
-    CORS(
-        app,
-        resources={
-            r"/*": {
-                "origins": [
-                    os.getenv("FRONTEND_URL", "http://localhost:3000"),
-                    "http://localhost:3000", 
-                    "http://127.0.0.1:3000", 
-                    "http://localhost:3001"
-                ],
-                "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-                "allow_headers": ["Content-Type", "Authorization", "Access-Control-Allow-Headers"]
-            }
-        },
-        supports_credentials=True
-    )
-    app.run(
-        host=host,
-        port=port,
-        debug=debug
-    )
 # import os
 # from dotenv import load_dotenv
-# from flask import Flask
-# from flask_cors import CORS
 # from app import create_app
+# from flask_cors import CORS
 
 # load_dotenv()
 # app = create_app()
 
-# CORS(
-#     app,
-#     resources={
-#         r"/predictions/*": {
-#             "origins": [
-#                 "http://localhost:3000",
-#                 "http://127.0.0.1:3000",
-#                 "http://localhost:3001",
-#             ],
-#             "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-#             "allow_headers": ["Content-Type", "Authorization"],
-#             "supports_credentials": True,
-#         }
-#     },
-# )
 
 # if __name__ == "__main__":
+
 #     host = os.getenv("HOST", "0.0.0.0")
 #     port = int(os.getenv("PORT", 5000))
 #     debug = os.getenv("FLASK_DEBUG", "False").lower() == "true"
 
-#     app.run(host=host, port=port, debug=debug)
+
+#     CORS(
+#         app,
+#         resources={
+#             r"/*": {
+#                 "origins": [
+#                     os.getenv("FRONTEND_URL", "http://localhost:3000"),
+#                     "http://localhost:3000", 
+#                     "http://127.0.0.1:3000", 
+#                     "http://localhost:3001"
+#                 ],
+#                 "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+#                 "allow_headers": ["Content-Type", "Authorization", "Access-Control-Allow-Headers"]
+#             }
+#         },
+#         supports_credentials=True
+#     )
+#     app.run(
+#         host=host,
+#         port=port,
+#         debug=debug
+#     )
+# # import os
+# # from dotenv import load_dotenv
+# # from flask import Flask
+# # from flask_cors import CORS
+# # from app import create_app
+
+# # load_dotenv()
+# # app = create_app()
+
+# # CORS(
+# #     app,
+# #     resources={
+# #         r"/predictions/*": {
+# #             "origins": [
+# #                 "http://localhost:3000",
+# #                 "http://127.0.0.1:3000",
+# #                 "http://localhost:3001",
+# #             ],
+# #             "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+# #             "allow_headers": ["Content-Type", "Authorization"],
+# #             "supports_credentials": True,
+# #         }
+# #     },
+# # )
+
+# # if __name__ == "__main__":
+# #     host = os.getenv("HOST", "0.0.0.0")
+# #     port = int(os.getenv("PORT", 5000))
+# #     debug = os.getenv("FLASK_DEBUG", "False").lower() == "true"
+
+# #     app.run(host=host, port=port, debug=debug)
+import os
+from dotenv import load_dotenv
+from flask_cors import CORS
+from app import create_app
+
+load_dotenv()
+
+app = create_app()
+
+CORS(
+    app,
+    resources={
+        r"/*": {
+            "origins": [
+                os.getenv("FRONTEND_URL", "http://localhost:3000"),
+                "http://localhost:3000",
+                "http://127.0.0.1:3000",
+                "http://localhost:3001",
+            ],
+            "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            "allow_headers": ["Content-Type", "Authorization"],
+        }
+    },
+    supports_credentials=True,
+)
+
+if __name__ == "__main__":
+    host = os.getenv("HOST", "0.0.0.0")
+    port = int(os.getenv("PORT", 5000))
+    debug = os.getenv("FLASK_DEBUG", "False").lower() == "true"
+
+    app.run(host=host, port=port, debug=debug)
